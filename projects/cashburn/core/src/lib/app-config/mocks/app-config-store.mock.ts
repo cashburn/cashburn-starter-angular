5d4fe1ec3765bb7a0d9ed8c73d6b99eda6845e0f
Colin Ashburn <cashburn26@gmail.com>
2026-01-04 21:55:40 -0600

    fix(test): Correct coverage paths and rename config to app-config

--- projects/cashburn/core/src/lib/app-config/mocks/app-config-store.mock.ts
+++ projects/cashburn/core/src/lib/app-config/mocks/app-config-store.mock.ts
@@ -1,9 +1,11 @@
+import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 
 import { AppConfigStore } from '../app-config-store';
 import { AppConfig } from '../models';
 import { mockAppConfig } from './app-config.mock';
 
+@Injectable()
 class MockAppConfigStore extends AppConfigStore {
     override loadConfig() {
         return of(mockAppConfig);
